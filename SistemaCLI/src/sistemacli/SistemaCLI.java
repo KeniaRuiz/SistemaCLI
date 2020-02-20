12d174b4ad10e2936a96b0c66cb700ea14c6c8c5
lochoas <galoochoa25@gmail.com>
2020-02-19 21:24:34 -0700

    Update SistemaCLI.java
    Version final 1.2

--- SistemaCLI/src/sistemacli/SistemaCLI.java
+++ SistemaCLI/src/sistemacli/SistemaCLI.java
@@ -58,35 +58,40 @@ public class SistemaCLI {
     }
 
     public static void fibonacci() {
+
         boolean verif = false;
         int limite = 0;
-        int n1 = 0;
-        int n2 = 1;
+        int a = 0;
+        int b = 1;
+        int c;
 
+        System.out.println("ALGORITMO DE FIBONACCI");
         while (!verif) {
-            System.out.println("ALGORITMO DE FIBONACCI");
-            System.out.print("Introduzca el número de términos para la serie de Fibonacci: ");
+            System.out.print("Introduzca el número limite para la serie de Fibonacci: ");
             String limiteS = scan.nextLine();
-            if (comprobarOpcion(limiteS)) {
+            if (comprobarOpcion(limiteS, 1)) {
                 verif = true;
                 limite = Integer.parseInt(limiteS);
+                if (limite <= 0) {
+                    System.out.println("El valor debe ser mayor a 0, escoga otro número");
+                    verif = false;
+                }
             }
-        }
-
-        while (limite == 0) {
-            System.out.println("No puede introducir el número 0, escoga otro número");
-            System.out.println("Introduzca un número mayor que 1: ");
-            limite = scan.nextInt();
         }
 
         System.out.println("===== Resultado =====");
 
-        System.out.print(n1 + " ");
-        for (int i = 2; i <= limite; i++) {
-            System.out.print(n2 + " ");
-            n2 = n1 + n2;
-            n1 = n2 - n1;
+        System.out.print(a + ", ");
+        System.out.print(b);
+
+        while (a + b <= limite) {
+            c = a;
+            a = b;
+            b = c + a;
+
+            System.out.print(", " + b);
         }
+
     }
 
     public static void padovan() {
@@ -97,9 +102,13 @@ public class SistemaCLI {
             System.out.println("Introduce hasta que número deseas obtener la sucesión de Padovan");
             System.out.println("Fin: ");
             String limiteS = scan.nextLine();
-            if (comprobarOpcion(limiteS)) {
+            if (comprobarOpcion(limiteS, 1)) {
                 verif = true;
                 limite = Integer.parseInt(limiteS);
+                if (limite <= 0) {
+                    System.out.println("El valor debe ser mayor a 0, escoga otro número");
+                    verif = false;
+                }
             }
         }
         System.out.println("===== Resultado =====");
@@ -107,17 +116,16 @@ public class SistemaCLI {
             System.out.print("1");
         } else if (limite < 2) {
             System.out.print("1, 1");
-        } else if (limite < 3) {
-            System.out.print("1, 1, 1");
         } else {
             System.out.print("1, 1, 1");
             int a = 1, b = 1, c = 1, d = 1;
-            for (int i = 3; i <= limite; i++) {
+            while (a + b <= limite) {
                 d = a + b;
                 a = b;
                 b = c;
                 c = d;
                 System.out.print(", " + d);
+
             }
         }
     }
@@ -125,67 +133,67 @@ public class SistemaCLI {
     public static void trianguloPascal() {
 
         boolean verif = false;
-        int limite = 0;
+        long limite = 0;
         while (!verif) {
             System.out.println("ALGORITMO DE TRIÁNGULO DE PASCAL");
             System.out.println("Introduce hasta que número deseas obtener el triángulo de Pascal: ");
             String limiteS = scan.nextLine();
-            if (comprobarOpcion(limiteS)) {
+            if (comprobarOpcion(limiteS, 2)) {
                 verif = true;
-                limite = Integer.parseInt(limiteS);
+                limite = Long.parseLong(limiteS);
+                if (limite <= 0 || limite > 150) {
+                    System.out.println("El valor debe ser mayor a 0 y como límite de 150, escoga otro número");
+                    verif = false;
+                }
             }
         }
 
-        while (limite == 0) {
-            System.out.println("No puede introducir el número 0, escoga otro número");
-            System.out.println("Introduzca un número mayor que 1: ");
-            limite = scan.nextInt();
-        }
+        System.out.println("===== Resultado =====");
+        long[] aux = new long[1];
 
-        if (limite > 150) {
-            System.out.println("El limite de columnas es de 150");
-            System.out.println("Introduzca un número menor: ");
-            limite = scan.nextInt();
+        for (long i = 1; i <= limite; i++) {
+            long[] pascal = new long[(int)i];
 
-        } else {
-
-            System.out.println("===== Resultado =====");
-            int[] aux = new int[1];
-
-            for (int i = 1; i <= limite; i++) {
-                int[] pascal = new int[i];
-
-                for (int k = limite; k > i; k--) {
-                    System.out.print(" ");
-                }
-
-                for (int j = 0; j < i; j++) {
-                    if (j == 0 || j == (i - 1)) {
-                        pascal[j] = 1;
-                    } else {
-                        pascal[j] = aux[j] + aux[j - 1];
-                    }
-                    System.out.print(pascal[j] + " ");
-                }
-
-                aux = pascal;
-                System.out.println();
-
+            for (long k = limite; k > i; k--) {
+                System.out.print(" ");
             }
+
+            for (long j = 0; j < i; j++) {
+                if (j == 0 || j == (i - 1)) {
+                    pascal[ (int) j] = 1;
+                } else {
+                    pascal[(int) j] = aux[(int) j] + aux[(int) j - 1];
+                }
+                System.out.print(pascal[(int)j] + " ");
+            }
+
+            aux = pascal;
+            System.out.println();
 
         }
 
     }
 
-    public static boolean comprobarOpcion(String num) {
+    public static boolean comprobarOpcion(String num, int tipo) {
         int numE;
+        long numEDos;
         boolean esEntero;
-        try {
-            numE = Integer.parseInt(num);
-            esEntero = true;
-        } catch (NumberFormatException e) {
-            System.out.println("Error!!! El valor debe ser númerico entero");
-            esEntero = false;
+        if (tipo == 1) {
+            try {
+                numE = Integer.parseInt(num);
+                esEntero = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Error!!! El valor debe ser númerico entero");
+                esEntero = false;
+            }
+        } else {
+            try {
+                numEDos = Long.parseLong(num);
+                esEntero = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Error!!! El valor debe ser númerico entero");
+                esEntero = false;
+            }
         }
         return esEntero;
     }
